9e89156c1b6c1254c6d98b85cf97bdeb6e08123a
artemy <artemy@SW-DEV12>
2019-06-10 11:17:34 +0300

    Edited basic VS Angular template so now I can write my awesome (no) application.

--- YaChess/ClientApp/src/app/app.component.ts
+++ YaChess/ClientApp/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'app';
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.router.navigate(['home']);
+  }
 }
--- YaChess/ClientApp/src/app/app.server.module.ts
+++ YaChess/ClientApp/src/app/app.server.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { ServerModule } from '@angular/platform-server';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 import { AppModule } from './app.module';
 
 @NgModule({
     imports: [AppModule, ServerModule, ModuleMapLoaderModule],
     bootstrap: [AppComponent]
 })
+
 export class AppServerModule { }

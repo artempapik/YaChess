7c1a9b6c598e07219dc7eb46b82fe7460bbe11e0
artemy <artemy@SW-DEV12>
2019-06-10 12:26:17 +0300

    First significant understanding of fugure app. Array of buttons. Change 'x' position on click.

--- YaChess/ClientApp/src/app/home/home.component.ts
+++ YaChess/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,47 @@
 import { Component } from '@angular/core';
+import { Figure } from '../services/figure';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html'
 })
 
-export class HomeComponent { }
+export class HomeComponent {
+  y: number = 100;
+
+  mainFigures: Figure[] = [];
+  secondaryFigures: Figure[] = [];
+
+  ngOnInit() {
+    let x: number = 100;
+    let y: number = 60;
+
+    for (let i: number = 0; i < 8; i++) {
+      this.mainFigures.push(new Figure(x, this.y));
+      this.secondaryFigures.push(new Figure(x, y));
+      x += 50;
+    }
+  }
+
+  changePosition1(i: number) {
+    document.getElementById(`mainFigure${i}`).style.left = `${--this.mainFigures[i].x}px`;
+  }
+
+  changePosition2(i: number) {
+    document.getElementById(`secondaryFigure${i}`).style.left = `${--this.secondaryFigures[i].x}px`;
+  }
+
+  setStyles1(i: number) {
+    return {
+      'left': `${this.mainFigures[i].x}px`,
+      'top': `${this.mainFigures[i].y}px`
+    };
+  }
+
+  setStyles2(i: number) {
+    return {
+      'left': `${this.secondaryFigures[i].x}px`,
+      'top': `${this.secondaryFigures[i].y}px`
+    };
+  }
+}
---
+++ YaChess/ClientApp/src/app/services/figure.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Figure = /** @class */ (function () {
+    function Figure(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return Figure;
+}());
+exports.Figure = Figure;
+//# sourceMappingURL=figure.js.map
